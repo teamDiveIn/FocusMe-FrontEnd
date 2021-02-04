91593a3446abccfa3200ad65a245d58eff4c077a
youngjae <elite1111cyj@gmail.com>
2021-02-05 01:29:49 +0900

    newpool component 완성

---
+++ src/pages/NewPool/contoller.js
@@ -0,0 +1,17 @@
+import { useForm } from 'antd/lib/form/Form'
+import { useCallback } from 'react'
+import { useHistory } from 'react-router-dom'
+
+export const useController = () => {
+  const [form] = useForm()
+  const history = useHistory()
+
+  const onSubmit = useCallback(async () => {
+    const updatedForm = form.getFieldsValue(true)
+
+    console.log(updatedForm)
+
+    history.replace('/register/complete')
+  }, [form, history])
+  return { form, onSubmit }
+}
