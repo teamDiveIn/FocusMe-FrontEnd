4c84a34540f89aa056a91d0a038b513f5c0baa3b
Beygee <doug0476@naver.com>
2021-02-05 01:01:35 +0900

    PoolListItemCard 추가

---
+++ src/pages/Pool/List/PoolListItemCard/index.jsx
@@ -0,0 +1,25 @@
+import { Divider } from 'antd'
+import * as B from 'src/components'
+import theme from 'src/styles/theme'
+export const PoolListItemCard = () => {
+  return (
+    <B.BaseCard radius="5px">
+      <B.Box display="flex" justify="space-between" align="center">
+        <B.Box>
+          <B.BaseText block size="big">
+            방제목입니당 공부해요
+          </B.BaseText>
+          <B.BaseText block size="small" type="optional">
+            영어자격증
+            <Divider type="vertical" style={{ borderLeft: `1px solid ${theme.disabled}` }} /> 자유
+            모드
+          </B.BaseText>
+          <B.BaseText block></B.BaseText>
+        </B.Box>
+        <B.Box>
+          <B.BaseText>5 / 6</B.BaseText>
+        </B.Box>
+      </B.Box>
+    </B.BaseCard>
+  )
+}
