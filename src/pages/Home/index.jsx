3998b1bd1f90ea2d61dcc3209a34b1f70f3ec04a
youngjae <elite1111cyj@gmail.com>
2021-02-05 00:48:13 +0900

    NewPool Page + Home 수정

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,38 @@
 import * as B from 'src/components'
+import { Link, Route } from 'react-router-dom'
+import { Input, Button, Typography } from 'antd'
+import theme from 'src/styles/theme'
+import { Fade } from 'react-reveal'
+
+const { Title } = Typography;
 
 const HomePage = () => {
-  return <B.BaseTemplate>
-    
+  return <B.BaseTemplate backgroundColor={theme.primaryOverlay} verticalCenter>
+
+    <B.Box mlr={4} width="100%">
+      <Fade bottom distance="20px" delay={150}>
+        <Link to="/newpool">
+          <B.BaseCard backgroundColor={theme.primaryDark} shadow radius={"50px"}>
+            <B.Box p={2}>
+              <Title level={3} style={{ color: 'white' }}>새로운 풀 만들기</Title>
+            </B.Box>
+          </B.BaseCard>
+        </Link>
+      </Fade>
+    </B.Box>
+
+    <B.Box mlr={4} width="100%">
+      <Fade bottom distance="20px" delay={150}>
+        <Link to="/poollist">
+          <B.BaseCard backgroundColor={theme.primaryDark} shadow radius={"50px"}>
+            <B.Box p={2}>
+              <Title level={3} style={{ color: 'white' }}>기존 풀 입장하기</Title>
+            </B.Box>
+          </B.BaseCard>
+        </Link>
+      </Fade>
+    </B.Box>
+
   </B.BaseTemplate>
 }
 
---
+++ src/pages/PoolList/index.jsx
@@ -0,0 +1,15 @@
+import * as B from 'src/components'
+import { Link, Route } from 'react-router-dom'
+import { Input, Button, Typography } from 'antd'
+import theme from 'src/styles/theme'
+import { Fade } from 'react-reveal'
+
+const { Title } = Typography;
+
+const NewPool = () => {
+  return <B.BaseTemplate backgroundColor={theme.primaryOverlay} verticalCenter>
+      
+  </B.BaseTemplate>
+}
+
+export default NewPool
