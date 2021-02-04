912b650660565c41c4d76eb904869f81f883ba61
Beygee <doug0476@naver.com>
2021-02-05 00:30:32 +0900

    상세 뷰 퍼블리싱

---
+++ src/pages/Pool/View/style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const StyledCardContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+`
+
+export const StyledCardWrapper = styled.div`
+  flex: 0 0 280px;
+  margin-bottom: 30px;
+`
