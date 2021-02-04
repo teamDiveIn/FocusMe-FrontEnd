57816bd055f996415eaef5ac5a5b310db9382f26
youngjae <elite1111cyj@gmail.com>
2021-02-04 21:41:06 +0900

    NewPool, MyPage page 추가

---
+++ src/pages/MyPage/index.jsx
@@ -0,0 +1,10 @@
+import * as B from 'src/components'
+import { Link, Route } from 'react-router-dom'
+
+const MyPage = () => {
+  return <B.BaseTemplate>
+      <p>MyPage</p>
+  </B.BaseTemplate>
+}
+
+export default MyPage
---
+++ src/pages/NewPool/index.jsx
@@ -0,0 +1,10 @@
+import * as B from 'src/components'
+import { Link, Route } from 'react-router-dom'
+
+const NewPool = () => {
+  return <B.BaseTemplate>
+      <p>NewPool</p>
+  </B.BaseTemplate>
+}
+
+export default NewPool
