8acdb5cb10abd07ac5b518d6b4dedaf72726f6f7
youngjae <elite1111cyj@gmail.com>
2021-02-04 23:11:15 +0900

    newpool 중간정리

--- src/pages/NewPool/index.jsx
+++ src/pages/NewPool/index.jsx
@@ -1,9 +1,18 @@
 import * as B from 'src/components'
 import { Link, Route } from 'react-router-dom'
+import Step1 from './step1'
+import Step2 from './step2'
+import Step3 from './step3'
 
 const NewPool = () => {
+  const step = 1;
   return <B.BaseTemplate>
-      <p>NewPool</p>
+      <h1>New pool</h1>
+      {step == 1 ?
+      <Step1/>
+      :
+      <Step2/>
+      }
   </B.BaseTemplate>
 }
 
