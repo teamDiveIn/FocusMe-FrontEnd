7e0812603dfd45360151dff0d60782951dade6d4
Beygee <doug0476@naver.com>
2021-02-05 20:04:46 +0900

    joinSession leaveSession 구현

--- src/pages/Pool/View/controller.js
+++ src/pages/Pool/View/controller.js
@@ -3,6 +3,7 @@ import { useCallback, useState } from 'react'
 import { message } from 'antd'
 import { useEffect } from 'react'
 import { nodeApiAxios } from 'src/lib/axios'
+import { OpenVidu } from 'openvidu-browser'
 
 export const useController = () => {
   const [visible, setVisible] = useState(true)
@@ -15,13 +16,115 @@ export const useController = () => {
     setVisible(false)
   }, [])
 
+  //////////////////////////////////////////////////////////////////////////////////////////////////////
+  const [, setOV] = useState()
+  const [sessionData, setSessionData] = useState({
+    sessionName: '',
+    session: undefined,
+    nickname: 'testNickname',
+    mainStreamManager: undefined,
+    publisher: undefined,
+    subscribers: [],
+    token: undefined,
+  })
+
+  useEffect(() => {
+    const OV = new OpenVidu()
+    setOV(OV)
+
+    const mySession = OV.initSession()
+    setSessionData({ ...sessionData, session: mySession })
+
+    mySession.on('streamCreated', (event) => {
+      const subscriber = mySession.subscribe(event.stream, undefined)
+      const subscribers = sessionData.subscribers
+      subscribers.push(subscriber)
+
+      setSessionData({ ...sessionData, subscribers })
+    })
+
+    const deleteSubscriber = (streamManager) => {
+      const subscribers = sessionData.subscribers
+      let index = subscribers.indexOf(streamManager, 0)
+      if (index > -1) {
+        subscribers.splice(index, 1)
+        setSessionData({ ...sessionData, subscribers })
+      }
+    }
+
+    mySession.on('streamDestroyed', (event) => {
+      deleteSubscriber(event.stream.streamManager)
+    })
+
+    async function run() {
+      const sessionName = 'test'
+      const { data } = await nodeApiAxios.post('/webrtc/token', {
+        session: sessionName,
+      })
+
+      const { token } = data
+
+      await mySession.connect(token, { clientData: {} })
+
+      const publisher = OV.initPublisher(undefined, {
+        audioSource: undefined, // The source of audio. If undefined default microphone
+        videoSource: undefined, // The source of video. If undefined default webcam
+        publishAudio: true, // Whether you want to start publishing with your audio unmuted or not
+        publishVideo: true, // Whether you want to start publishing with your video enabled or not
+        resolution: '640x480', // The resolution of your video
+        frameRate: 30, // The frame rate of your video
+        insertMode: 'APPEND', // How the video is inserted in the target element 'video-container'
+        mirror: false, // Whether to mirror your local video or not
+      })
+
+      mySession.publish(publisher)
+
+      // Set the main video in the page to display our webcam and store our Publisher
+      setSessionData({
+        ...sessionData,
+        mainStreamManager: publisher,
+        publisher,
+        token,
+        sessionName,
+      })
+    }
+
+    run()
+    // eslint-disable-next-line
+  }, [])
+
+  const leaveSession = useCallback(async () => {
+    const mySession = sessionData.session
+
+    if (mySession) {
+      mySession.disconnect()
+    }
+
+    setOV(undefined)
+    setSessionData({
+      session: undefined,
+      subscribers: [],
+      mySessionId: 'SessionA',
+      myUserName: 'Participant' + Math.floor(Math.random() * 100),
+      mainStreamManager: undefined,
+      publisher: undefined,
+    })
+
+    // TODO: 자리를 뜰 때 node서버에
+    await nodeApiAxios.delete('/webrtc/token', {
+      session: sessionData.sessionName,
+      token: sessionData.token,
+    })
+  }, [sessionData.session, sessionData.token, sessionData.sessionName])
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////
+
   // 테스트 웹 연결
   useEffect(() => {
     async function run() {
       // const { data } = await nodeApiAxios.post('/webrtc/token', {
       //   session: 'test',
       // })
-
       // console.log(data)
     }
 
@@ -110,5 +213,5 @@ export const useController = () => {
     }
   }
 
-  return { init, onToggleVisible, visible, onClose }
+  return { init, onToggleVisible, visible, onClose, leaveSession }
 }
