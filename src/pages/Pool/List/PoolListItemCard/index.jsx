a87765a89a503c978379c434b37326584d14e4de
youngjae <elite1111cyj@gmail.com>
2021-02-06 08:24:06 +0900

    pool list 항목 수정

--- src/pages/Pool/List/PoolListItemCard/index.jsx
+++ src/pages/Pool/List/PoolListItemCard/index.jsx
@@ -3,7 +3,7 @@ import { useHistory } from 'react-router-dom'
 import * as B from 'src/components'
 import theme from 'src/styles/theme'
 
-export const PoolListItemCard = () => {
+export const PoolListItemCard = ({title, keyword, mode, peoplestatus}) => {
   const history = useHistory()
   return (
     <B.Box mb={2}>
@@ -11,17 +11,16 @@ export const PoolListItemCard = () => {
         <B.Box display="flex" justify="space-between" align="center">
           <B.Box>
             <B.BaseText block size="big">
-              방제목입니당 공부해요
+              {title}
             </B.BaseText>
             <B.BaseText block size="small" type="optional">
               영어자격증
-              <Divider type="vertical" style={{ borderLeft: `1px solid ${theme.disabled}` }} /> 자유
-              모드
+              <Divider type="vertical" style={{ borderLeft: `1px solid ${theme.disabled}` }} /> { mode===1 ? ("자유모드"): ("조용한모드")}
             </B.BaseText>
             <B.BaseText block></B.BaseText>
           </B.Box>
           <B.Box>
-            <B.BaseText>5 / 6</B.BaseText>
+            <B.BaseText>{peoplestatus}</B.BaseText>
           </B.Box>
         </B.Box>
       </B.BaseCard>
