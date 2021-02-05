7c0e55991a0c2db4e1d4aa02b1c44e569af903f2
Beygee <doug0476@naver.com>
2021-02-05 12:33:40 +0900

    ML 삽입

--- src/pages/Pool/View/index.jsx
+++ src/pages/Pool/View/index.jsx
@@ -1,13 +1,22 @@
 import * as B from 'src/components'
 import * as S from './style'
 import { PoolCamCard } from './PoolCamCard'
+import { useController } from './controller'
+import { useEffect } from 'react'
 
 const PoolViewPage = () => {
+  const { init } = useController()
+  useEffect(() => {
+    init()
+    // eslint-disable-next-line
+  }, [])
   return (
     <B.BaseTemplate>
       <B.BaseText bold type="white" size={32} block mb={4}>
         영어 자격증 방
       </B.BaseText>
+      <div id="label-container"></div>
+      <canvas id="canvas"></canvas>
       <S.StyledCardContainer>
         <S.StyledCardWrapper>
           <PoolCamCard />
