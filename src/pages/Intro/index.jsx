d1971c6b964f4ebe591bfb8b80aad93eb1c1f53b
youngjae <elite1111cyj@gmail.com>
2021-02-05 23:08:33 +0900

    intro set 2

--- src/pages/Intro/index.jsx
+++ src/pages/Intro/index.jsx
@@ -31,31 +31,39 @@ const IntroPage = () => {
         <img src="/images/introPage/pattern2.png" alt="" className="img-fluid" style={{ width: "100%" }} />
       </section>
       <main id="main" style={{ marginLeft: "20%", marginRight: "20%" }}>
-
         <section id="about">
-          <div className="container">
+          <div className="container" style={{ height: "300px", textAlign: "center" }}>
+            <div className="about-content">
+              <div style={{ display: "block", marginBottom: "30px" }}>
+                <img src="/images/Logo_t3.png" style={{ width: "300px", display: "inline", verticalAlign: "middle", marginBottom: "30px" }}></img>
+                <h3 style={{ display: "inline", verticalAlign: "middle", marginTop: "10px" }}>은 이런 분들에게 도움이 될 수 있습니다!</h3>
+              </div>
+              <ul>
+                <li>
+                  <p>카페나 독서실, 모르는 사람들과 한 공간에 함께해야 집중이 잘 되는 카공족</p>
+                </li>
+                <li>
+                  <p>공부하고자 하는 분야에 대해 정보가 부족해 도움이 필요한 입문자</p>
+                </li>
+                <li>
+                  <p>친구들과 무조건 함께! 얼굴을 보며 공부해야 집중이 잘 되는 줌터디족</p>
+                </li>
+                <li>
+                  <p>더 나아가 서비스가 확장 된다면 초등학생들의 학습관 교정을 위한 공교육에도 활용될 수 있습니다</p>
+                </li>
+              </ul>
+
+            </div>
+          </div>
+        </section>
+
+        <section id="ourteam">
+          <div className="container" style={{ height: "300px", textAlign: "center" }}>
             <div className="row">
               <div className="col-lg-7 col-md-6">
                 <div className="about-content">
-                  <div style={{display:"block", marginBottom:"30px"}}>
-                    <img src="/images/Logo_t3.png" style={{width:"300px",display:"inline", verticalAlign:"middle", marginRight:"20px", marginBottom:"30px"}}></img>
-                    <h3 style={{display:"inline" , verticalAlign:"middle", marginTop:"10px"}}>은 이런 분들에게 도움이 될 수 있습니다!</h3>
-                  </div>
-                  
-                  <ul style={{marginLeft:"100px"}}>
-                    <li>
-                      <p>카페나 독서실, 모르는 사람들과 한 공간에 함께해야 집중이 잘 되는 카공족</p>
-                    </li>
-                    <li>
-                      <p>공부하고자 하는 분야에 대해 정보가 부족해 도움이 필요한 입문자</p>
-                    </li>
-                    <li>
-                      <p>친구들과 무조건 함께! 얼굴을 보며 공부해야 집중이 잘 되는 줌터디족</p>
-                    </li>
-                    <li>
-                      <p>더 나아가 서비스가 확장 된다면 초등학생들의 학습관 교정을 위한 공교육에도 활용될 수 있습니다</p>
-                    </li>
-                  </ul>
+                  <h2 style={{ position: "relative", marginBottom: "50px" }}>온라인 스터디룸인 POOL에 다이빙해 비슷한 목표를 가진 사람들과 마음껏 헤엄쳐보세요 </h2>
+                  <p>다이브인의 자세한 기능이 궁금하다면?</p>
                 </div>
               </div>
             </div>
@@ -63,12 +71,12 @@ const IntroPage = () => {
         </section>
 
         <section id="ourteam">
-          <div className="container">
+          <div className="container" style={{ height: "300px", textAlign: "center" }}>
             <div className="row">
               <div className="col-lg-7 col-md-6">
                 <div className="about-content">
-                  <h2 style={{position:"relative"}}>About Team</h2>
-                  
+                  <h2 style={{ position: "relative" }}>About Team</h2>
+
                 </div>
               </div>
             </div>
